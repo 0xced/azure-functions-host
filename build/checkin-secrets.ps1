4459b92317b3211bb9123a6ea39d7fe65c6f8d54
Brett Samblanet <brettsam@microsoft.com>
2020-01-09 10:41:35 -0800

    updates

--- build/checkin-secrets.ps1
+++ build/checkin-secrets.ps1
@@ -17,7 +17,7 @@ if ($leaseToken -eq "") {
 Write-Host "Breaking lease for $leaseBlob."
 
 $storageContext = New-AzStorageContext -ConnectionString $connectionString
-$blob = Get-AzureStorageBlob -Context $storageContext -Container "ci-locks" -Blob $leaseBlob
+$blob = Get-AzStorageBlob -Context $storageContext -Container "ci-locks" -Blob $leaseBlob
 
 $accessCondition = New-Object -TypeName Microsoft.Azure.Storage.AccessCondition
 $accessCondition.LeaseId = $leaseToken
